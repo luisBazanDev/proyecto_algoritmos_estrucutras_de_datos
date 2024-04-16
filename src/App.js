ceb9b46e71e5f887ed90a2ddc340c8ad8bb8ace8
Luis Bazan <luis@bazan.pe>
2024-04-16 11:32:02 -0500

    Install: - tailwindcss - axios Created: - AppContext
    and implement AppContext

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import AppContext from "./contexts/AppContext";
+
 function App() {
+  const [dataState, setDataState] = useState({});
+  const [data, setData] = useState({});
+  const [page, setPage] = useState(1);
+  const [token, setToken] = useState("");
+
+  const value = {
+    dataState,
+    setDataState,
+    data,
+    setData,
+    page,
+    setPage,
+    token,
+    setToken,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">Hello world!</header>
-    </div>
+    <AppContext.Provider value={value}>
+      <div className="App">
+        <header className="App-header">Hello world!</header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
