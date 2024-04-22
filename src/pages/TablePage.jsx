e204a31e4a8ddd6415c0826a5bb37c44bb898961
Luis Bazan <luis@bazan.pe>
2024-04-22 14:10:48 -0500

    Table page test

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -3,7 +3,28 @@ import Table from "../components/content/Table";
 import UpperBar from "../components/algorithms/UpperBar";
 
 function TablePage() {
-  return <div>TablePage</div>;
+  return (
+    <div className="flex flex-col">
+      <div>Controls</div>
+      <div className="flex overflow-x-auto">
+        {[
+          "col1",
+          "col2",
+          "col3",
+          "col4",
+          "col5",
+          "col6",
+          "col7",
+          "col8",
+          "col9",
+        ].map((col, index) => (
+          <div className="min-w-48 text-center" key={index}>
+            {col}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TablePage;
