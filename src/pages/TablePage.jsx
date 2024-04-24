e9cd94b933e36deadb260dfc834de12b56697b32
Luis Bazan <luis@bazan.pe>
2024-04-24 11:51:38 -0500

    Completed status request

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { useEffect, useContext } from "react";
+import axios from "axios";
 import TableComponent from "../components/TableComponent";
 import SortComponent from "../components/SortComponent";
 import SearchComponent from "../components/SearchComponent";
+import AppContext from "../contexts/AppContext";
 
 function TablePage() {
+  const { token, setDataState } = useContext(AppContext);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios
+        .get("/api/status", { headers: { Authorization: `${token}` } })
+        .then((res) => {
+          setDataState(res.data.tableStatus);
+        });
+    }, 1000);
+    return interval;
+  }, [setDataState, token]);
+
   return (
     <div className="flex flex-col h-full">
       <div className="flex justify-between text-white bg-gray-900">
